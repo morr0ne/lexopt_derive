d8b81a28756fd1e22bc79e285400cdca137c265a
Matilde Morrone <contact@morrone.dev>
2023-08-19 20:24:58 +0200

    Added example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,23 @@
+use std::{
+    ffi::OsString,
+    io::{stdout, Write},
+    path::PathBuf,
+};
+
+use lexopt::Error;
 use lexopt_derive::Parser;
 
-#[derive(Parser)]
-struct Args {}
+#[derive(Debug, Parser)]
+struct Args {
+    name: String,
+    path: Option<PathBuf>,
+}
 
-fn main() {}
+fn main() -> Result<(), Error> {
+    let args = std::env::args_os();
+    let args = Args::parse(args, "Help message")?;
+
+    dbg!(args);
+
+    Ok(())
+}
